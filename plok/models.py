fa5dda326a95e25c3734c6c2ff64a09a7da72071
Jarno Luoma-Nirva <jln@iki.fi>
2017-09-10 00:01:27 +0300

    DB:Add Blog and Article models

--- plok/models.py
+++ plok/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+# from django.contrib.auth.models import User
+from django.contrib import auth
+from django.utils.translation import ugettext_lazy
+from django.conf import settings  # For available languages
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
+class Blog(models.Model):
+    name = models.SlugField(max_length=100, unique=True, verbose_name=ugettext_lazy('name'),
+                            help_text=ugettext_lazy('Must be unique. Used in URL.'))
+    title = models.CharField(max_length=250, verbose_name=ugettext_lazy('title'))
+    description = models.TextField(null=True, blank=True, verbose_name=ugettext_lazy('description'))
+    language = models.CharField(max_length=50, choices=settings.LANGUAGES, default='en',
+                                verbose_name=ugettext_lazy('language'))
+    created = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(auth.get_user_model(), related_name='blog_created_by')
+    edited = models.DateTimeField(auto_now=True)
+    edited_by = models.ForeignKey(auth.get_user_model(), null=True, related_name='blog_edited_by')
+
+    def articles(self):
+        return Article.objects.filter(blog=self)
+
+    def can_edit(self, user):
+        if user == self.created_by:
+            return True
+
+        return False
+
+    def __unicode__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('plok:blog', args=[self.name])
+
+
+class Article(models.Model):
+    blog = models.ForeignKey(Blog, null=False)
+    name = models.SlugField(max_length=100, unique=True, verbose_name=ugettext_lazy('name'),
+                            help_text=ugettext_lazy('Must be unique. Used in URL.'))
+    title = models.CharField(max_length=250, verbose_name=ugettext_lazy('title'))
+    description = models.TextField(null=True, blank=True,  # Short description, used in page meta headers
+                                   verbose_name=ugettext_lazy('description'))
+    text = models.TextField(null=True, blank=True, verbose_name=ugettext_lazy('text'))  # Actual blog content
+    language = models.CharField(max_length=50, choices=settings.LANGUAGES, default='en',
+                                verbose_name=ugettext_lazy('language'))
+    created = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(auth.get_user_model(), related_name='article_created_by')
+    edited = models.DateTimeField(auto_now=True)
+    edited_by = models.ForeignKey(auth.get_user_model(), null=True, related_name='article_edited_by')
+
+    def can_edit(self, user):
+        if user == self.created_by:
+            return True
+
+        return False
+
+    def __unicode__(self):
+        return self.blog.name + ':' + self.name
+
+    def get_absolute_url(self):
+        return reverse('plok:article', args=[self.blog.name, self.name])
+
+    def get_edit_url(self):
+        return reverse('plok:article_update', args=[self.blog.name, self.name])
+
+    class Meta:
+        ordering = ['-created']  # Default ordering by points (descending)
---
+++ plok/tests/test_plokkeri_models.py
@@ -0,0 +1,51 @@
+#from unittest import skip
+from django.db import IntegrityError
+from django.contrib import auth
+from plok.models import Blog, Article
+from .ext_test_case import ExtTestCase
+
+
+class BlogTests(ExtTestCase):
+    def can_save_and_load_blog(self):
+        creator = auth.get_user_model().objects.create(username='creator')
+        self.assertEqual(Blog.objects.all().count(), 0)
+        blog = Blog(created_by=creator, name="test_blog")
+        blog.save()
+        self.assertEqual(Blog.objects.all().count(), 1)
+
+    def test_saving_blog_without_name_or_creator_fails(self):
+        blog = Blog()
+        with self.assertRaises(IntegrityError):
+            blog.save()
+
+    def test_saving_blog_with_existing_name_fails(self):
+        creator = auth.get_user_model().objects.create(username='creator')
+        blog1 = Blog(created_by=creator, name="test_blog")
+        blog1.save()
+        self.assertEqual(Blog.objects.all().count(), 1)
+        blog2 = Blog(created_by=creator, name="test_blog")
+        with self.assertRaises(IntegrityError):
+            blog2.save()
+
+    def test_blog_articles(self):
+        creator = auth.get_user_model().objects.create(username='creator')
+        blog = Blog.objects.create(created_by=creator, name="test_blog", title="Test blog")
+        self.assertEqual(blog.articles().count(), 0)
+        article = Article.objects.create(blog=blog, name="test_article", title="Test article", created_by=creator)
+        self.assertEqual(blog.articles().count(), 1)
+        self.assertEqual(blog.articles()[0], article)
+
+
+class ArticleTests(ExtTestCase):
+    def can_save_and_load_article(self):
+        creator = auth.get_user_model().objects.create(username='creator')
+        blog = Blog.objects.create(created_by=creator, name="test_blog")
+        self.assertEqual(Article.objects.all().count(), 0)
+        article = Article(blog=blog, created_by=creator, slug="test_article")
+        article.save()
+        self.assertEqual(Article.objects.all().count(), 1)
+
+    def test_saving_article_without_name_or_creator_fails(self):
+        article = Article()
+        with self.assertRaises(IntegrityError):
+            article.save()
