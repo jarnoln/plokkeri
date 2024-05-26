f313d7a680b263bc1b7e7822cbf29377f58c6dbe
Jarno Luoma-Nirva <jarno.luomanirva@infiniteqsystems.com>
2024-05-26 13:42:19 +0300

    Add DEFAULT_AUTO_FIELD-setting
    Required by Django since version 3.2

--- plokkeri/settings.py
+++ plokkeri/settings.py
@@ -116,6 +116,9 @@ DATABASES = {
     }
 }
 
+# Needed since Django 3.2:
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
